dc4fb7b241625e06996fa7d9a4125cb35be42820
Tanmay Nagare <tanmaynagare2204@gmail.com>
2025-08-01 18:45:03 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+
+console.log("tanmay")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Null is object but undefined is a type 
